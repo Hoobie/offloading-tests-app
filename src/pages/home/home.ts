e52d4b4b155e559d43f37c34d0e41dc36bf21de3
Hoobie <hoobie92@gmail.com>
2017-02-03 18:27:43 +0100

    Add a button for running all tasks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,44 +11,48 @@ import * as Rx from 'rxjs';
 export class HomePage {
 
   output = "";
+  tasks = [];
 
   constructor(public navCtrl: NavController, public plt: Platform) {
+    let t = this;
+
+    let onBatteryStatus = function(status) {
+      console.debug("Level: " + status.level + " isPlugged: " + status.isPlugged);
+      t.log("[BATTERY] Level: " + status.level);
+    };
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+
+    let cpuTask = new Task(
+      Rx.Observable.create(function(observer) {
+        let a = 0;
+        for (let i = 0; i < 100000000; i++) {
+          a = Math.sqrt(i);
+        }
+        t.log("[CPU] Calculated the result: " + a);
+        observer.complete();
+      }), 50000);
+    this.tasks.push(cpuTask);
+
+    let wifiTask = new Task(
+      Rx.Observable.create(function(observer) {
+        var ld = function() {
+          t.log("[WiFi] Downloaded the image");
+          observer.complete();
+        };
+
+        var img = new Image();
+        img.onload = ld;
+        img.src = "http://www.tipsforlawyers.com/wp-content/uploads/2014/08/networking-links.jpg?" + Math.random() + '=' + new Date();
+      }), 50000);
+    this.tasks.push(wifiTask);
+  }
+
+  runAllTasks() {
     this.plt.ready().then(() => {
-      let tasks = [];
+      this.log("[DEBUG] Running all the tasks");
       let t = this;
 
-      let onBatteryStatus = function(status) {
-        console.debug("Level: " + status.level + " isPlugged: " + status.isPlugged);
-        t.log("[BATTERY] Level: " + status.level);
-      };
-      window.addEventListener("batterystatus", onBatteryStatus, false);
-
-      let cpuTask = new Task(
-        Rx.Observable.create(function(observer) {
-          let a = 0;
-          for (let i = 0; i < 100000000; i++) {
-            a = Math.sqrt(i);
-          }
-          t.log("[CPU] Calculated the result: " + a);
-          observer.complete();
-        }), 20000);
-      tasks.push(cpuTask);
-
-      let wifiTask = new Task(
-        Rx.Observable.create(function(observer) {
-          var ld = function() {
-            t.log("[WiFi] Downloaded the image");
-            observer.complete();
-          };
-
-          var img = new Image();
-          img.onload = ld;
-          img.src = "http://www.tipsforlawyers.com/wp-content/uploads/2014/08/networking-links.jpg?" + Math.random() + '=' + new Date();
-        }), 20000);
-      tasks.push(wifiTask);
-
-      this.log("[DEBUG] Running all the tasks");
-      let tasksRunner = new TasksRunner(tasks);
+      let tasksRunner = new TasksRunner(this.tasks);
       tasksRunner.runAll().subscribe(function(data) {
         t.log("[DEBUG] The task is done");
       }, function(err) { }, function() {
